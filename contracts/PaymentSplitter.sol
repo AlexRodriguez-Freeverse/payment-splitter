637a11b05cfb373a31542c73aa6718ac739deef7
Alex Rodriguez <alex.rodriguez@freeverse.io>
2023-06-02 23:43:51 +0200

    Storage removed since all the members are now params in a payable function

--- contracts/PaymentSplitter.sol
+++ contracts/PaymentSplitter.sol
@@ -2,44 +2,32 @@
 pragma solidity ^0.8.0;
 
 contract PaymentSplitter {
-    address payable[] public recipients;
-    uint[] public amounts;
-    address payable public defaultRecipient;
-    
-    constructor() {}
-    
-    receive() external payable {
-        require(defaultRecipient != address(0), "PaymentSplitter: Default recipient not initialized.");
-        require(recipients.length == amounts.length && recipients.length > 0, "PaymentSplitter: Recipients and amounts not initialized.");
-        require(address(this).balance >= getTotalAmount(), "PaymentSplitter: Insufficient balance to pay all recipients.");
-        
-        for (uint i = 0; i < recipients.length; i++) {
-            require(amounts[i] > 0, "PaymentSplitter: Amount is too small to send.");
-            recipients[i].transfer(amounts[i]);
-        }
-        
-        uint remainder = address(this).balance;
-        if (remainder > 0) {
-            defaultRecipient.transfer(remainder);
+    // Split the received payment among the recipients
+    // The recipients and amounts are provided as parameters everytime UPlay wants to share the commissions among the referrals
+    // Note that the default address to get the rest of the payments should be included in the recipients
+    // TODO: Check if the caller is allowed to do this
+    function receiveAndSplit(address payable[] memory _recipients, uint[] memory _amounts) external payable {
+        // Ensure that the recipients and amounts are initialized and have equal lengths
+        require(_recipients.length == _amounts.length && _recipients.length > 0, "PaymentSplitter: Recipients and amounts not initialized.");
+
+        // Ensure that the received amount matches the total amount to be distributed
+        require(msg.value == getTotalAmount(_amounts), "PaymentSplitter: Incorrect payment amount.");
+
+        // Iterate through the recipients and transfer the corresponding amounts
+        for (uint i = 0; i < _recipients.length; i++) {
+            // Ensure that the amount is greater than zero.
+            // TODO: Check if it is a real problem
+            require(_amounts[i] > 0, "PaymentSplitter: Amount is too small to send.");
+            _recipients[i].transfer(_amounts[i]);
         }
     }
     
-    function getTotalAmount() public view returns (uint) {
+    // Calculate the total amount from an array of amounts
+    function getTotalAmount(uint[] memory _amounts) internal pure returns (uint) {
         uint totalAmount = 0;
-        for (uint i = 0; i < amounts.length; i++) {
-            totalAmount += amounts[i];
+        for (uint i = 0; i < _amounts.length; i++) {
+            totalAmount += _amounts[i];
         }
         return totalAmount;
-    }
-    
-    function setRecipientsAndAmounts(address payable[] memory _recipients, uint[] memory _amounts) public {
-        require(_recipients.length == _amounts.length, "PaymentSplitter: Unequal array lengths");
-        recipients = _recipients;
-        amounts = _amounts;
-    }
-    
-    function setDefaultRecipient(address payable _defaultRecipient) public {
-        require(_defaultRecipient != address(0), "PaymentSplitter: Default recipient cannot be zero address");
-        defaultRecipient = _defaultRecipient;
     }
 }
